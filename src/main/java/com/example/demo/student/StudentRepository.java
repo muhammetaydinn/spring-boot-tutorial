5e3962e656fc4b66b0cbbfce09422bd0b275e614
Muhammet Aydın <muhammetaydin1704@gmail.com>
2023-09-04 03:52:09 +0300

    Writing Businnes Logic

--- src/main/java/com/example/demo/student/StudentRepository.java
+++ src/main/java/com/example/demo/student/StudentRepository.java
@@ -3,7 +3,16 @@ package com.example.demo.student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 //jpa repo lib ini extend ettikten sonra < class , ve id tipi>
 @Repository
 public interface StudentRepository extends JpaRepository<Student,Long> {
+
+    //custom functions
+    // Optional tpiinde
+    // select * from student where email = ?
+    // findStudentBy yazdıktan sonra email i otomatik algıladı. Daha da ozelligi var
+    // kendi fonksiyonunu direkt üretti
+    Optional<Student> findStudentByEmail(String email);
 }
