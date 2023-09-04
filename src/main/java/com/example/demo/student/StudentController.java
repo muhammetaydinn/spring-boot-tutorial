299562795c5f94cebb68ba9a06f551aabcbd5f74
Muhammet Aydın <muhammetaydin1704@gmail.com>
2023-09-04 03:35:40 +0300

    PostMapping

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,9 +1,7 @@
 package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,6 +20,14 @@ public class StudentController {
     @GetMapping
     public List<Student> getStudents( ){
         return  studentService.getStudents();
-
+    }
+    // post map için ornek ogrenci kaydı
+    // bu fonksiyonu PostMapping ile tanımlarız
+    // ogrenci datası girmek icin Student i @RequestBody tipinde tanımlarız
+    //içine de servisimiz. { istediğimizfonksiyonuadı}  yazar hata gelinc
+    // servise create method deriz
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student){
+        studentService.addNewStudent(student);
     }
 }
